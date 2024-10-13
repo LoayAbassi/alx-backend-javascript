91032c0f3d3417f9d87b9790bf0d77c440b707aa
LoayAbassi <abassi.loay@gmail.com>
2024-10-13 14:38:03 +0100

    eslint :(

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,5 +1,6 @@
-export default function getResponseFromAPI(){
-    return new Promise((resolve,reject)=>{
-        resolve();
-    });
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    resolve();
+    reject();
+  });
 }
