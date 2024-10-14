1982fed289178b87152900fe7ab6c0ac0e9dfdc4
LoayAbassi <abassi.loay@gmail.com>
2024-10-14 21:12:12 +0100

    lint, always lint

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,15 +1,13 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName,lastName,fileName){
-    return Promise.allSettled([
-        signUpUser(firstName,lastName),
-        uploadPhoto(fileName)
-    ]).then((result)=>(
-        result.map((r)=>({
-            status:r.status,
-            value:r.status === "fulfilled"?r.value:String(r.reason),
-        })))
-    )
-
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((result) => (
+    result.map((r) => ({
+      status: r.status,
+      value: r.status === 'fulfilled' ? r.value : String(r.reason),
+    }))));
 }
